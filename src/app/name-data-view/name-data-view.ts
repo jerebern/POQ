d940ab2adc24b535f98477888cf2c9985a5e4eff
Jérémy Bernard <jere.bern@hotmail.com>
2025-10-02 20:36:24 -0400

    UI detail nom (#10)
    * graph améliorer
    
    * présentation année améliorer
    
    * Ordre sur le tableau info année
    
    * Onglet

--- src/app/name-data-view/name-data-view.ts
+++ src/app/name-data-view/name-data-view.ts
@@ -7,13 +7,13 @@ import { MatExpansionModule} from '@angular/material/expansion';
 import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
 import * as echarts from 'echarts/core';
 import {  LineChart } from 'echarts/charts';
-import { GridComponent } from 'echarts/components';
+import { GridComponent, TitleComponent, TooltipComponent } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
 import { EChartsCoreOption } from 'echarts/core';
-echarts.use([LineChart, GridComponent, CanvasRenderer])
+echarts.use([LineChart, GridComponent, CanvasRenderer, TooltipComponent,TitleComponent])
 @Component({
   selector: 'app-name-data-view',
-  imports: [MatListModule, MatDividerModule, MatIconModule,MatExpansionModule,NgxEchartsDirective],
+  imports: [MatListModule, MatDividerModule, MatIconModule, MatExpansionModule, NgxEchartsDirective],
   templateUrl: './name-data-view.html',
   styleUrl: './name-data-view.scss',
   providers: [provideEchartsCore({ echarts })],
@@ -23,8 +23,13 @@ export class NameDataView implements OnInit{
   @Input() nameData : NameData|null = null
   yearsData : string[] = []
   yearsValue : number[] =[]
+  chartOption: EChartsCoreOption|null = null
+  useOrder : boolean = true
+  yearOrder : boolean = true
+
   ngOnInit(): void {
     this.extractNameData()
+    this.initChart()
   }
     grayBackground(rowIndex : number){
     if(rowIndex%2 == 1){
@@ -34,6 +39,7 @@ export class NameDataView implements OnInit{
       return "grayRow"
     }
   }
+  
   extractNameData(){
 
     for(let year of this.nameData!.years){
@@ -41,21 +47,48 @@ export class NameDataView implements OnInit{
       this.yearsValue.push(year.value)
     }
   }
-  chartOption: EChartsCoreOption = {
-  xAxis: {
-    type: 'category',
-    data: this.yearsData
-  },
-  yAxis: {
-    type: 'value',
-  },
-  series: [
-    {
-      data: this.yearsValue,
-      type: 'line',
-    },
-  ],
-};
 
+  initChart(){
+      this.chartOption = {
+      title: {
+            text: this.nameData!.name + " ( "+ this.nameData!.nameType  + " ) \n Utilisation total depuis 1980 : " + this.nameData!.totalUse,
+      },
+        tooltip: {},
+      xAxis: {
+        type: 'category',
+        data: this.yearsData
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name:"Année",
+          data: this.yearsValue,
+          type: 'line',
+        },
+      ],
+    };
+
+  }
+orderByUse(){
+      if(!this.useOrder){
+      this.nameData!.years = this.nameData!.years.sort((a,b) => a.value - b.value)
+    }
+    else{
+      this.nameData!.years = this.nameData!.years.sort((a,b) => b.value - a.value)
+    }
+      this.useOrder = !this.useOrder
+}
+
+orderByYear(){
+    if(!this.yearOrder){
+      this.nameData!.years = this.nameData!.years.sort((a,b) => Number(a.digits) - Number(b.digits))
+    }
+    else{
+      this.nameData!.years = this.nameData!.years.sort((a,b) => Number(b.digits) - Number(a.digits))
+    }
+      this.yearOrder = !this.yearOrder
+}
 
 }
