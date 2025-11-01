cd34a4eb95089901c37f9a13c526783d25bb248d
Félix Carle-Milette <60113848+felixcm1129@users.noreply.github.com>
2025-11-01 08:43:00 -0400

    UI date dernière mise à jour des données (#13)
    * matToolTip pour la date
    
    * correction

--- src/app/services/local-storage-service.ts
+++ src/app/services/local-storage-service.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@angular/core';
 import { NameData } from '../objects/name-data';
+import { last } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalStorageService {
   validateWebDataBase() : boolean{
-    let storeValue =this.getLastWebDataBaseUpdate()
+    let storeValue = this.getLastWebDataBaseUpdate()
     let lastUpdate : Date
+    let futurDate = new Date()
+    futurDate.setDate(futurDate.getDate() + 7)
+
     if(storeValue != null){
       lastUpdate = new Date(storeValue)
-      if(new Date().getDay() < lastUpdate.getDay() + 7){
+      if(futurDate < lastUpdate){
         return true
       }
     }
     return false
   }
+
   setDisclamer(){
     localStorage.setItem("disclamer",new Date().toDateString())
   }
+
   getDisclamer(){
     return localStorage.getItem("disclamer")
   }
@@ -26,9 +32,16 @@ export class LocalStorageService {
   getLastWebDataBaseUpdate(){
     return localStorage.getItem("lastUpdate")
   }
+
+  getLastUpdateDate(){
+    let updateDate = new Date(localStorage.getItem("lastUpdate")?.toString() ?? "")
+    return updateDate.getDate() + "/" + (updateDate.getMonth()+1) + "/" + updateDate.getFullYear()
+  }
+
   setLastWebdataBaseUpdate(){
     localStorage.setItem("lastUpdate",new Date().toDateString())
   }
+
   clearLocalStorage(){
     localStorage.clear()
   }
