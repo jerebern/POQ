0837c8959ae3d917e55020a8bcb172f384161c4f
Jerebern <jere.bern@hotmail.com>
2026-05-14 21:58:57 -0400

    filtre

--- src/app/name-data-table/name-data-table.ts
+++ src/app/name-data-table/name-data-table.ts
@@ -7,7 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { CdkColumnDef } from '@angular/cdk/table';
 import { MatInputModule } from '@angular/material/input';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { NameType } from '../../enum/nameType';
 import { TableFilterType } from '../../enum/table-filter-type';
 import replaceSpecialCharacters from 'replace-special-characters';
@@ -15,217 +15,215 @@ import { LocalDbService } from '../services/local-db-service';
 
 @Component({
   selector: 'app-name-data-table',
-  imports: [NameDataRow,MatIconModule,MatButtonModule,MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule,ReactiveFormsModule],
-  providers:[CdkColumnDef],
+  imports: [
+    NameDataRow,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [CdkColumnDef],
   templateUrl: './name-data-table.html',
-  styleUrl: './name-data-table.scss'
+  styleUrl: './name-data-table.scss',
 })
-export class NameDataTable implements OnInit{
-  constructor(
-        private localDbService : LocalDbService
-  ){}
-  nameDatas : NameData[] = [] //TODO COMPORTEMENT ICI A REFACTORISER
+export class NameDataTable implements OnInit {
+  constructor(private localDbService: LocalDbService) {}
+  nameDatas: NameData[] = []; //TODO COMPORTEMENT ICI A REFACTORISER
   @Output() viewEvent = new EventEmitter<NameData>();
-  changeTypeIndex : number = 0
-  orderType : boolean = false
-  totalUseOrder : boolean = true
-  alphabeticalOrderName : boolean = false
-  filteredNoms : NameData[] = []
-  nameDataArrayIndex : number = 0
-  pageIndex : number = 0
-  typeFormControl = new FormControl("ALL")
-  filterdNameFormControl = new FormControl()
-  filterdNameWihtoutSpecialChar : string = ""
-  selectedFilters : TableFilterType[] = []
-  typeHommeIndex : number = 0
-  typeFemmeIndex : number = 0
-  indexbound = 100
+  changeTypeIndex: number = 0;
+  orderType: boolean = false;
+  totalUseOrder: boolean = true;
+  alphabeticalOrderName: boolean = false;
+  filteredNoms: NameData[] = [];
+  nameDataArrayIndex: number = 0;
+  pageIndex: number = 0;
+  typeFormControl = new FormControl('ALL');
+  filterdNameFormControl = new FormControl();
+  filterdNameWihtoutSpecialChar: string = '';
+  selectedFilters: TableFilterType[] = [];
+  typeHommeIndex: number = 0;
+  typeFemmeIndex: number = 0;
+  indexbound = 100;
   async ngOnInit() {
-    await this.initNameData()
-    this.initFormsSub()
-    this.setnameDataFromPageIndex()
-    this.orderByTotalOfUse(false)
-
+    await this.initNameData();
+    this.initFormsSub();
+    this.setnameDataFromPageIndex();
+    this.orderByTotalOfUse(false);
   }
 
-  initFormsSub(){
-    this.typeFormControl.valueChanges.subscribe(
-      (value)=>{
-        if(value != "ALL"){
-          this.addFilter(TableFilterType.TYPE)
-        }
-        else{
-          this.removeFilter(TableFilterType.TYPE)
-        }
-        this.setnameDataFromPageIndex(undefined,true)
+  initFormsSub() {
+    this.typeFormControl.valueChanges.subscribe((value) => {
+      if (value != 'ALL') {
+        this.addFilter(TableFilterType.TYPE);
+      } else {
+        this.removeFilter(TableFilterType.TYPE);
       }
-    )
-    this.filterdNameFormControl.valueChanges.subscribe(
-      value=>{
-        if(value == null || value == ""){
-          this.removeFilter(TableFilterType.NAME)
-        }
-        else{
-          this.filterdNameWihtoutSpecialChar = replaceSpecialCharacters(value)
-          this.addFilter(TableFilterType.NAME)
-        }
-      this.setnameDataFromPageIndex(undefined,true)
+      this.setnameDataFromPageIndex(undefined, true);
+    });
+    this.filterdNameFormControl.valueChanges.subscribe((value) => {
+      if (value == null || value == '') {
+        this.removeFilter(TableFilterType.NAME);
+      } else {
+        this.filterdNameWihtoutSpecialChar = replaceSpecialCharacters(value);
+        this.searchName(this.filterdNameWihtoutSpecialChar);
 
+        this.addFilter(TableFilterType.NAME);
       }
-    )
+      this.setnameDataFromPageIndex(undefined, true);
+    });
   }
-  onViewEvent(nameData : NameData){
-    this.viewEvent.emit(nameData)
+  onViewEvent(nameData: NameData) {
+    this.viewEvent.emit(nameData);
   }
-  addFilter(filterType :TableFilterType){
-    if(!this.selectedFilters.find((element) => element == filterType)){
-      this.selectedFilters.push(filterType)
+  addFilter(filterType: TableFilterType) {
+    if (!this.selectedFilters.find((element) => element == filterType)) {
+      this.selectedFilters.push(filterType);
     }
-      if(filterType == TableFilterType.TYPE){
-        if(this.typeFormControl.value == NameType.FEMME){
-          this.nameDataArrayIndex = Number(this.typeFemmeIndex) //Constructeur number pour éviter les pointeur
-        }
-        else{
-          this.nameDataArrayIndex = Number(this.typeHommeIndex)
-        }
+    if (filterType == TableFilterType.TYPE) {
+      if (this.typeFormControl.value == NameType.FEMME) {
+        this.nameDataArrayIndex = Number(this.typeFemmeIndex); //Constructeur number pour éviter les pointeur
+      } else {
+        this.nameDataArrayIndex = Number(this.typeHommeIndex);
       }
+    }
   }
-  removeFilter(filterType :TableFilterType){
-    this.selectedFilters =  this.selectedFilters.splice(this.selectedFilters.findIndex((element)=> element == filterType),0)
-    this.nameDataArrayIndex = 0
-
+  removeFilter(filterType: TableFilterType) {
+    this.selectedFilters = this.selectedFilters.splice(
+      this.selectedFilters.findIndex((element) => element == filterType),
+      0,
+    );
+    this.nameDataArrayIndex = 0;
   }
-  get disableNextButton(){
-    return this.selectedFilters.find((element) => element == TableFilterType.NAME)
+  get disableNextButton() {
+    return this.selectedFilters.find((element) => element == TableFilterType.NAME);
   }
-    get disablePreviousButton(){
-    if(this.pageIndex > 0){
+  get disablePreviousButton() {
+    if (this.pageIndex > 0) {
       return false;
     }
     return true;
   }
 
-  setnameDataFromPageIndex(action ?: string, resetPageIndex ?: boolean){
-    (this.nameDatas.length)
-    this.filteredNoms = []
-    if(action == "NEXT" && this.nameDatas.length <= this.nameDatas.length + this.indexbound ){
-      this.nameDataArrayIndex += this.indexbound
-      this.pageIndex++
-
+  setnameDataFromPageIndex(action?: string, resetPageIndex?: boolean) {
+    this.nameDatas.length;
+    this.filteredNoms = [];
+    if (action == 'NEXT' && this.nameDatas.length <= this.nameDatas.length + this.indexbound) {
+      this.nameDataArrayIndex += this.indexbound;
+      this.pageIndex++;
+    } else if (action == 'PREVIOUS' && this.nameDataArrayIndex > 0) {
+      this.nameDataArrayIndex -= this.indexbound;
+      this.pageIndex--;
     }
-    else if (action == "PREVIOUS" && this.nameDataArrayIndex > 0){
-        this.nameDataArrayIndex -= this.indexbound
-        this.pageIndex--
-    }
-    if(resetPageIndex){
-      this.pageIndex = 0
+    if (resetPageIndex) {
+      this.pageIndex = 0;
     }
 
-    let index = structuredClone(this.nameDataArrayIndex)
-    while(index < this.nameDatas.length){
-      if(this.applyFilter(this.nameDatas[index])){
-      this.filteredNoms.push(this.nameDatas[index])
+    let index = structuredClone(this.nameDataArrayIndex);
+    while (index < this.nameDatas.length) {
+      if (this.applyFilter(this.nameDatas[index])) {
+        this.filteredNoms.push(this.nameDatas[index]);
       }
-      if(this.filteredNoms.length > 99){
-        break
+      if (this.filteredNoms.length > 99) {
+        break;
       }
-      index++
+      index++;
     }
   }
 
-  resetIndexFromFilter(){
-    this.pageIndex = 0
-    this.pageIndex = 0
+  resetIndexFromFilter() {
+    this.pageIndex = 0;
+    this.pageIndex = 0;
   }
 
-  async initNameData(){
-    this.resetIndexFromFilter()
-    this.nameDatas = await this.localDbService.getNamesDatas()
-    this.typeFemmeIndex = this.nameDatas.findIndex((element)=> element.nameType == NameType.FEMME)
-    this.typeHommeIndex = this.nameDatas.findIndex((element)=> element.nameType == NameType.HOMME)
+  async initNameData() {
+    this.resetIndexFromFilter();
+    this.nameDatas = await this.localDbService.getNamesDatas();
+    this.typeFemmeIndex = this.nameDatas.findIndex((element) => element.nameType == NameType.FEMME);
+    this.typeHommeIndex = this.nameDatas.findIndex((element) => element.nameType == NameType.HOMME);
   }
 
-  applyFilter(name : NameData){
-    let successCondition : number = 0
-    if(this.selectedFilters.length == 0){
-      return true
-    }
-    else{
+  async searchName(value: string) {
+    this.nameDatas = await this.localDbService.searchNameDatas(value);
+    console.log(this.nameDatas);
+  }
+
+  applyFilter(name: NameData) {
+    let successCondition: number = 0;
+    if (this.selectedFilters.length == 0) {
+      return true;
+    } else {
       //pourrait être simplifier
-      for( let filter of this.selectedFilters){
-        if(filter == TableFilterType.TYPE && name.nameType == this.typeFormControl.value){
-        successCondition++
+      for (let filter of this.selectedFilters) {
+        if (filter == TableFilterType.TYPE && name.nameType == this.typeFormControl.value) {
+          successCondition++;
         }
-       if(filter == TableFilterType.NAME && name.name?.toUpperCase().match(this.filterdNameWihtoutSpecialChar.toUpperCase())) {
-        successCondition++
-        }
+        /*         if (
+          filter == TableFilterType.NAME &&
+          name.name?.toUpperCase().match(this.filterdNameWihtoutSpecialChar.toUpperCase())
+        ) {
+          successCondition++;
+        } */
       }
-      if(successCondition == this.selectedFilters.length){
-        return true
+      if (successCondition == this.selectedFilters.length) {
+        return true;
       }
     }
-    return false
+    return false;
   }
-  orderByTotalOfUse(resetData ?: boolean){
-    if(resetData){
-    this.initNameData()
+  orderByTotalOfUse(resetData?: boolean) {
+    if (resetData) {
+      this.initNameData();
     }
-    if(!this.totalUseOrder){
-      this.nameDatas = this.nameDatas.sort((a,b) => a.totalUse - b.totalUse)
+    if (!this.totalUseOrder) {
+      this.nameDatas = this.nameDatas.sort((a, b) => a.totalUse - b.totalUse);
+    } else {
+      this.nameDatas = this.nameDatas.sort((a, b) => b.totalUse - a.totalUse);
     }
-    else{
-      this.nameDatas = this.nameDatas.sort((a,b) => b.totalUse - a.totalUse)
-    }
-    this.totalUseOrder = !this.totalUseOrder
-    this.setnameDataFromPageIndex(undefined,true)
+    this.totalUseOrder = !this.totalUseOrder;
+    this.setnameDataFromPageIndex(undefined, true);
   }
-  compareStringAlphabetical(nameA : string, nameB : string){
-     if (nameA < nameB) {
-       return -1;
-     }
-     if (nameA > nameB) {
-       return 1;
-     }
+  compareStringAlphabetical(nameA: string, nameB: string) {
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
     return 0;
   }
 
-  orderByAlphabetical(){
-    this.initNameData()
+  orderByAlphabetical() {
+    this.initNameData();
     this.nameDatas.sort((a, b) => {
-      if(!this.alphabeticalOrderName){
-       return this.compareStringAlphabetical(a.name!.toUpperCase(),b.name!.toUpperCase())
+      if (!this.alphabeticalOrderName) {
+        return this.compareStringAlphabetical(a.name!.toUpperCase(), b.name!.toUpperCase());
+      } else {
+        return this.compareStringAlphabetical(b.name!.toUpperCase(), a.name!.toUpperCase());
       }
-      else{
-       return this.compareStringAlphabetical(b.name!.toUpperCase(),a.name!.toUpperCase())
-      }
-    }
-
-  )
-    this.alphabeticalOrderName =!this.alphabeticalOrderName
-    this.setnameDataFromPageIndex(undefined,true)
+    });
+    this.alphabeticalOrderName = !this.alphabeticalOrderName;
+    this.setnameDataFromPageIndex(undefined, true);
   }
-  orderByType(){
-    this.initNameData()
+  orderByType() {
+    this.initNameData();
     this.nameDatas.sort((a, b) => {
-      if(!this.orderType){
-       return this.compareStringAlphabetical(a.nameType!.toUpperCase(),b.nameType!.toUpperCase())
+      if (!this.orderType) {
+        return this.compareStringAlphabetical(a.nameType!.toUpperCase(), b.nameType!.toUpperCase());
+      } else {
+        return this.compareStringAlphabetical(b.nameType!.toUpperCase(), a.nameType!.toUpperCase());
       }
-      else{
-       return this.compareStringAlphabetical(b.nameType!.toUpperCase(),a.nameType!.toUpperCase())
-      }
-    }
-
-  )
-    this.orderType = !this.orderType
-    this.setnameDataFromPageIndex()
+    });
+    this.orderType = !this.orderType;
+    this.setnameDataFromPageIndex();
   }
-  grayBackground(rowIndex : number){
-    if(rowIndex%2 == 1){
-      return ""
-    }
-    else{
-      return "grayRow"
+  grayBackground(rowIndex: number) {
+    if (rowIndex % 2 == 1) {
+      return '';
+    } else {
+      return 'grayRow';
     }
   }
-
 }
--- src/app/services/local-db-service.ts
+++ src/app/services/local-db-service.ts
@@ -1,28 +1,31 @@
 import { inject, Injectable } from '@angular/core';
 import { NameData } from '../objects/name-data';
 import { AppDb } from './app-db';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalDbService {
-  constructor(){}
+  constructor() {}
   dbService = inject(AppDb);
 
-async saveNamesData(
-  namesDatas: NameData[],
-  resetDatabase: boolean
-) {
+  async saveNamesData(namesDatas: NameData[], resetDatabase: boolean) {
+    if (resetDatabase) {
+      await this.dbService.namesDatas.clear();
+    }
 
-  if (resetDatabase) {
-
-    await this.dbService.namesDatas.clear();
+    await this.dbService.namesDatas.bulkAdd(namesDatas);
   }
 
-  await this.dbService.namesDatas.bulkAdd(namesDatas);
-}
-  async getNamesDatas() : Promise<any> {
+  async searchNameDatas(searchValue: string) {
+    return await this.dbService.namesDatas
+      //filtre
+      .where('name')
+      .startsWithAnyOfIgnoreCase(searchValue.toLocaleUpperCase())
+      .toArray();
+  }
+  async getNamesDatas(): Promise<any> {
     return await this.dbService.namesDatas.toArray();
   }
-
 }
